2118a427eef348f690a00b3d429b9c74e93dc2e5
Peter Meissner <petermeissner@users.noreply.github.com>
2019-05-24 10:14:18 +0200

    Update inst_r_sys.bash

--- inst_r_sys.bash
+++ inst_r_sys.bash
@@ -11,22 +11,22 @@ echo_headline start
 # updating
 echo_headline update/upgrade
 
-sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt update
+sudo apt -y upgrade
 
 
 
 # base r installation with dependencies for XML, xml2, RCurl, ... etc
 echo_headline R base / R dev / R dependencies
 
-sudo apt-get install -y r-base
-sudo apt-get install -y r-base-dev
-sudo apt-get install -y pandoc
-sudo apt-get install -y libssl-dev
-sudo apt-get install -y libxml2-dev
-sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y libssl-dev
+sudo apt install -y r-base
+sudo apt install -y r-base-dev
+sudo apt install -y pandoc
+sudo apt install -y libssl-dev
+sudo apt install -y libxml2-dev
+sudo apt install -y libssl-dev
 sudo apt install -y libv8-dev
-sudo apt-get install -y libcurl4-openssl-dev
+sudo apt install -y libcurl4-openssl-dev
+sudo apt install -y libpq-dev
 
-sudo apt-get -y install --no-install-recommends qpdf gfortran
+sudo apt -y install --no-install-recommends qpdf gfortran
